72411f309247cf8d5a28ae926a31f5967435d49f
deviZi <devis.arapi1@gmail.com>
2025-05-03 19:11:21 +0200

    added adding data to gorse subject

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -5,17 +5,31 @@ import { Subject } from '../entities/subject.entity';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 import { BulkCreateSubjectDto } from './dto/bulk-create-subject.dto';
+import { GorseService } from '../gorse/gorse.service';
 
 @Injectable()
 export class SubjectsService {
   constructor(
     @InjectRepository(Subject)
     private subjectsRepository: Repository<Subject>,
+    private gorseService: GorseService,
   ) {}
 
   async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
     const subject = this.subjectsRepository.create(createSubjectDto);
-    return this.subjectsRepository.save(subject);
+    const savedSubject = await this.subjectsRepository.save(subject);
+
+    // Insert into Gorse
+    await this.gorseService.insertItem({
+      ItemId: `subject:${savedSubject.id}`,
+      IsHidden: false,
+      Categories: ['1'],
+      Timestamp: new Date().toISOString(),
+      Labels: savedSubject.tags,
+      Comment: savedSubject.name,
+    });
+
+    return savedSubject;
   }
 
   async bulkCreate(
@@ -24,7 +38,21 @@ export class SubjectsService {
     const subjects = this.subjectsRepository.create(
       bulkCreateSubjectDto.subjects,
     );
-    return this.subjectsRepository.save(subjects);
+    const savedSubjects = await this.subjectsRepository.save(subjects);
+
+    // Insert into Gorse
+    const gorseItems = savedSubjects.map((subject) => ({
+      ItemId: `subject:${subject.id}`,
+      IsHidden: false,
+      Categories: ['4'],
+      Timestamp: new Date().toISOString(),
+      Labels: subject.tags,
+      Comment: subject.name,
+    }));
+
+    await this.gorseService.insertItems(gorseItems);
+
+    return savedSubjects;
   }
 
   async findAll(): Promise<Subject[]> {
