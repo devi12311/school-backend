cafdd7fd5321e29ffc5aa08b572294afb394c384
devis <devis@paperclicks.net>
2025-05-14 13:31:39 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: ['http://localhost:5173', 'https://tek-front.n7ujc3.easypanel.host'],
-      preflightContinue: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed methods
       credentials: true, // Allow credentials (e.g., cookies)
-      allowedHeaders: 'Content-Type, Accept', // Allowed headers
+      // allowedHeaders: 'Content-Type, Accept', // Allowed headers
     }
   });
   await app.listen(process.env.PORT ?? 3000);
