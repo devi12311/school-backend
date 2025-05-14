b995e55d7fb3d0d0fcb098a8d74d9720bd0080cf
devis <devis@paperclicks.net>
2025-05-14 11:04:48 +0200

    cors config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+      preflightContinue: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+      credentials: true, // Allow credentials (e.g., cookies)
+      allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    }
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
